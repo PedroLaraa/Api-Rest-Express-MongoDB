c7d9854350a7d24026b7852e8e89cd02c0f9cfac
Pedro Lara <pedroalveslara@gmail.com>
2022-09-19 13:53:07 -0300

    att

--- src/app.js
+++ src/app.js
@@ -2,14 +2,20 @@
 const http = require('http');
 const port = 3000;
 
+const rotas = {
+    '/': 'Curso NodeJS',
+    '/livros': 'Página de Livros',
+    '/autores': 'Listagem de Autores'
+};
+
 const server = http.createServer((req, res) => {
 
     res.writeHead(200, {'Content-Type': 'text/plain'});
 
-    res.end('Curso de NodeJs');
+    res.end(rotas[req.url]);
 
 });
 
 server.listen(port, () => {
-    console.log('Server rodando na porta: ', port);
+    console.log('Server rodando na porta:', port);
 });
